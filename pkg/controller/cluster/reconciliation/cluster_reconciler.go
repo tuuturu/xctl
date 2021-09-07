3abe02be0629abd4c2676220cb1eabeb453730b7
Julius Pedersen <deifyed@tutanota.com>
2021-09-07 16:58:17 +0200

    ✅ Add kubeconfig handling to cluster reconciler

--- pkg/controller/cluster/reconciliation/cluster_reconciler.go
+++ pkg/controller/cluster/reconciliation/cluster_reconciler.go
@@ -1,41 +1,70 @@
 package reconciliation
 
 import (
+	"context"
 	"fmt"
 
 	"github.com/deifyed/xctl/pkg/cloud"
+	"github.com/deifyed/xctl/pkg/config"
 	"github.com/deifyed/xctl/pkg/controller/common/reconciliation"
+	"github.com/spf13/afero"
 )
 
 type clusterReconciler struct {
 	clusterService cloud.ClusterService
 }
 
-func (c *clusterReconciler) Reconcile(ctx reconciliation.Context) (reconciliation.Result, error) {
-	action := reconciliation.DetermineUserIndication(ctx, true)
+func (c *clusterReconciler) Reconcile(rctx reconciliation.Context) (reconciliation.Result, error) {
+	action := reconciliation.DetermineUserIndication(rctx, true)
+
+	clusterExists, err := c.clusterService.HasCluster(rctx.Ctx, rctx.ClusterDeclaration.Metadata.Name)
+	if err != nil {
+		return reconciliation.Result{}, fmt.Errorf("checking cluster existence: %w", err)
+	}
 
 	switch action {
 	case reconciliation.ActionCreate:
-		err := c.clusterService.CreateCluster(ctx.Ctx, ctx.ClusterDeclaration)
+		if !clusterExists {
+			err = c.clusterService.CreateCluster(rctx.Ctx, rctx.ClusterDeclaration)
+			if err != nil {
+				return reconciliation.Result{}, fmt.Errorf("creating cluster: %w", err)
+			}
+		}
+
+		err = generateKubeconfig(rctx.Ctx, rctx.Filesystem, c.clusterService, rctx.ClusterDeclaration.Metadata.Name)
 		if err != nil {
-			return reconciliation.Result{}, fmt.Errorf("creating cluster: %w", err)
+			return reconciliation.Result{}, fmt.Errorf("generating kubeconfig: %w", err)
 		}
 
 		return reconciliation.Result{Requeue: false}, nil
 	case reconciliation.ActionDelete:
-		err := c.clusterService.DeleteCluster(ctx.Ctx, ctx.ClusterDeclaration.Metadata.Name)
+		if !clusterExists {
+			return reconciliation.Result{Requeue: false}, nil
+		}
+
+		err := c.clusterService.DeleteCluster(rctx.Ctx, rctx.ClusterDeclaration.Metadata.Name)
 		if err != nil {
 			return reconciliation.Result{}, fmt.Errorf("deleting cluster: %w", err)
 		}
 
 		return reconciliation.Result{Requeue: false}, nil
-	case reconciliation.ActionWait:
-		return reconciliation.Result{Requeue: true}, nil
-	case reconciliation.ActionNoop:
-		return reconciliation.Result{Requeue: false}, nil
 	}
 
 	return reconciliation.Result{}, reconciliation.ErrIndecisive
+}
+
+func generateKubeconfig(ctx context.Context, fs *afero.Afero, provider cloud.ClusterService, clusterName string) error {
+	rawConfig, err := provider.GetKubeConfig(ctx, clusterName)
+	if err != nil {
+		return fmt.Errorf("getting kubeconfig: %w", err)
+	}
+
+	err = fs.WriteFile(config.GetAbsoluteKubeconfigPath(), rawConfig, 0o744)
+	if err != nil {
+		return fmt.Errorf("writing kubeconfig: %w", err)
+	}
+
+	return nil
 }
 
 func (c *clusterReconciler) String() string {
