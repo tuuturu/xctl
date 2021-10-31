32d055b62088017fbd0406b4888a36b76b9ff181
Julius Pedersen <deifyed@tutanota.com>
2021-10-31 19:05:11 +0100

    ✅ Add root domain and admin email to cluster api

--- pkg/apis/xctl/v1alpha1/cluster.go
+++ pkg/apis/xctl/v1alpha1/cluster.go
@@ -2,10 +2,15 @@ package v1alpha1
 
 const ClusterKind = "Cluster"
 
+type ClusterSpec struct {
+	RootDomain string `json:"rootDomain"`
+	AdminEmail string `json:"adminEmail"`
+}
+
 type Cluster struct {
 	TypeMeta `json:",inline"`
-	Metadata Metadata `json:"metadata"`
-	URL      string   `json:"url"`
+	Metadata Metadata    `json:"metadata"`
+	Spec     ClusterSpec `json:"spec"`
 }
 
 func NewCluster() Cluster {
