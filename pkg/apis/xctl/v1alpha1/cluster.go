4db6662a46cbfe8a42048ce27924286dd7ff08af
Julius Pedersen <deifyed@tutanota.com>
2021-11-27 12:26:19 +0100

    ✅ Add plugins field to cluster manifest

--- pkg/apis/xctl/v1alpha1/cluster.go
+++ pkg/apis/xctl/v1alpha1/cluster.go
@@ -2,9 +2,16 @@ package v1alpha1
 
 const ClusterKind = "Cluster"
 
+type Plugins struct {
+	CertBot                bool `json:"certBot"`
+	NginxIngressController bool `json:"nginxIngressController"`
+	Vault                  bool `json:"vault"`
+}
+
 type ClusterSpec struct {
-	RootDomain string `json:"rootDomain"`
-	AdminEmail string `json:"adminEmail"`
+	RootDomain string  `json:"rootDomain"`
+	AdminEmail string  `json:"adminEmail"`
+	Plugins    Plugins `json:"plugins"`
 }
 
 type Cluster struct {
@@ -18,6 +25,13 @@ func NewCluster() Cluster {
 		TypeMeta: TypeMeta{
 			Kind:       ClusterKind,
 			APIVersion: apiVersion,
+		},
+		Spec: ClusterSpec{
+			Plugins: Plugins{
+				CertBot:                true,
+				NginxIngressController: true,
+				Vault:                  true,
+			},
 		},
 	}
 }
