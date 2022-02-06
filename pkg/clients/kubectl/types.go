b22990a987f18ec0dfe9c3dce5876892991b5316
Julius Pedersen <deifyed@tutanota.com>
2022-02-06 18:35:09 +0100

    👌 Clean up kubectl client interface

--- pkg/clients/kubectl/types.go
+++ pkg/clients/kubectl/types.go
@@ -35,11 +35,15 @@ type Pod struct {
 // StopFn knows how to stop things
 type StopFn func() error
 
-type Client interface {
+type Poder interface {
 	// PodExec executes a script within a specified pod
 	PodExec(PodExecOpts, ...string) error
 	// PortForward opens a port forwarding connection and returns a function to close that connection
 	PortForward(PortForwardOpts) (StopFn, error)
+}
+
+type Client interface {
+	Poder
 	// Apply applies a manifest to the contextual cluster
 	Apply(ApplyOpts) error
 }
