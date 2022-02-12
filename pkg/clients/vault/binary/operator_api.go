a49f60eb38ea346245fdaacecc59df77adb627be
Julius Pedersen <deifyed@tutanota.com>
2022-02-12 13:26:03 +0100

    👌 Clean up vault binary client structure

--- pkg/clients/vault/binary/operator_api.go
+++ pkg/clients/vault/binary/operator_api.go
@@ -65,3 +65,26 @@ func (c *client) Unseal(key string) error {
 
 	return nil
 }
+
+func (c *client) EnableKv2() error {
+	cmd := exec.Command(c.vaultPath, "secrets", "enable", "kv-v2")
+
+	stderr := bytes.Buffer{}
+	stdout := bytes.Buffer{}
+
+	cmd.Env = c.envAsArray()
+	cmd.Stderr = &stderr
+	cmd.Stdout = &stdout
+
+	err := cmd.Run()
+	if err != nil {
+		logrus.WithFields(logrus.Fields{
+			"stdout": stdout.String(),
+			"stderr": stderr.String(),
+		}).Debug("executing command")
+
+		return fmt.Errorf("executing command: %w", err)
+	}
+
+	return nil
+}
--- pkg/clients/vault/binary/secrets_api.go
+++ pkg/clients/vault/binary/secrets_api.go
@@ -1,40 +1,9 @@
 package binary
 
-import (
-	"bytes"
-	"fmt"
-	"os/exec"
-
-	"github.com/sirupsen/logrus"
-)
-
-func (c *client) EnableKv2() error {
-	cmd := exec.Command(c.vaultPath, "secrets", "enable", "kv-v2")
-
-	stderr := bytes.Buffer{}
-	stdout := bytes.Buffer{}
-
-	cmd.Env = c.envAsArray()
-	cmd.Stderr = &stderr
-	cmd.Stdout = &stdout
-
-	err := cmd.Run()
-	if err != nil {
-		logrus.WithFields(logrus.Fields{
-			"stdout": stdout.String(),
-			"stderr": stderr.String(),
-		}).Debug("executing command")
-
-		return fmt.Errorf("executing command: %w", err)
-	}
-
+func (c *client) Put(_ string, _ map[string]string) error {
 	return nil
 }
 
-func (c *client) Put(path string, values map[string]string) error {
-	return nil
-}
-
-func (c *client) Get(path string) (map[string]string, error) {
-	return nil, nil
+func (c *client) Get(_ string, _ string) (string, error) {
+	return "", nil
 }
