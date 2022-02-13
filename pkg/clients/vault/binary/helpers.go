0f8afb4d3ad6d986ee58f243ed194a2a55684827
Julius Pedersen <deifyed@tutanota.com>
2022-02-13 08:42:15 +0100

    ✅ Add vault client put/get secret functionality

--- pkg/clients/vault/binary/helpers.go
+++ pkg/clients/vault/binary/helpers.go
@@ -65,3 +65,16 @@ func (c *client) envAsArray() []string {
 
 	return env
 }
+
+func attributesAsArray(m map[string]string) []string {
+	result := make([]string, len(m))
+	index := 0
+
+	for key, value := range m {
+		result[index] = fmt.Sprintf("%s=%s", key, value)
+
+		index++
+	}
+
+	return result
+}
--- pkg/clients/vault/binary/secrets_api.go
+++ pkg/clients/vault/binary/secrets_api.go
@@ -1,9 +1,94 @@
 package binary
 
-func (c *client) Put(_ string, _ map[string]string) error {
+import (
+	"bytes"
+	"fmt"
+	"os/exec"
+	"strings"
+
+	"github.com/sirupsen/logrus"
+)
+
+func (c *client) Put(name string, secretAttributes map[string]string) error {
+	cmd := exec.Command(c.vaultPath, //nolint:gosec
+		"kv",
+		"put",
+		fmt.Sprintf("secret/%s", name),
+		strings.Join(attributesAsArray(secretAttributes), " "),
+	)
+
+	stderr := bytes.Buffer{}
+	stdout := bytes.Buffer{}
+
+	cmd.Env = c.envAsArray()
+	cmd.Stderr = &stderr
+	cmd.Stdout = &stdout
+
+	err := cmd.Run()
+	if err != nil {
+		logrus.WithFields(logrus.Fields{
+			"stdout": stdout.String(),
+			"stderr": stderr.String(),
+		}).Debug("executing command")
+
+		return fmt.Errorf("executing command: %w", err)
+	}
+
 	return nil
 }
 
-func (c *client) Get(_ string, _ string) (string, error) {
-	return "", nil
+func (c *client) Get(name string, key string) (string, error) {
+	cmd := exec.Command(c.vaultPath, //nolint:gosec
+		"kv",
+		"get",
+		"-field", key,
+		fmt.Sprintf("secret/%s", name),
+	)
+
+	stderr := bytes.Buffer{}
+	stdout := bytes.Buffer{}
+
+	cmd.Env = c.envAsArray()
+	cmd.Stderr = &stderr
+	cmd.Stdout = &stdout
+
+	err := cmd.Run()
+	if err != nil {
+		logrus.WithFields(logrus.Fields{
+			"stdout": stdout.String(),
+			"stderr": stderr.String(),
+		}).Debug("executing command")
+
+		return "", fmt.Errorf("executing command: %w", err)
+	}
+
+	return stdout.String(), nil
+}
+
+func (c *client) Delete(name string) error {
+	cmd := exec.Command(c.vaultPath, //nolint:gosec
+		"kv",
+		"metadata",
+		"delete",
+		fmt.Sprintf("secret/%s", name),
+	)
+
+	stderr := bytes.Buffer{}
+	stdout := bytes.Buffer{}
+
+	cmd.Env = c.envAsArray()
+	cmd.Stderr = &stderr
+	cmd.Stdout = &stdout
+
+	err := cmd.Run()
+	if err != nil {
+		logrus.WithFields(logrus.Fields{
+			"stdout": stdout.String(),
+			"stderr": stderr.String(),
+		}).Debug("executing command")
+
+		return fmt.Errorf("executing command: %w", err)
+	}
+
+	return nil
 }
