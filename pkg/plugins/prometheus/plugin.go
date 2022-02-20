c50e80e9c029cc0ac0dfee0d37990f6423684536
Julius Pedersen <deifyed@tutanota.com>
2022-02-20 08:48:40 +0100

    👌 Move chart URL to standardized location

--- pkg/plugins/prometheus/plugin.go
+++ pkg/plugins/prometheus/plugin.go
@@ -6,14 +6,13 @@ import (
 	"github.com/deifyed/xctl/pkg/apis/xctl/v1alpha1"
 )
 
-const pluginName = "prometheus"
-
 func NewPlugin() v1alpha1.Plugin {
 	plugin := v1alpha1.NewPlugin(pluginName)
 
 	plugin.Metadata.Name = pluginName
 	plugin.Metadata.Namespace = "kube-system"
 
+	// URL: https://artifacthub.io/packages/helm/prometheus-community/prometheus/15.1.3
 	plugin.Spec.Helm.Chart = "prometheus"
 	plugin.Spec.Helm.Version = "15.1.3"
 	plugin.Spec.Helm.Values = template
@@ -26,3 +25,5 @@ func NewPlugin() v1alpha1.Plugin {
 
 //go:embed values.yaml
 var template string //nolint:gochecknoglobals
+
+const pluginName = "prometheus"
