63dd26e1094b9725b3bca61d6f73b7c30afe454c
Julius Pedersen <deifyed@tutanota.com>
2022-05-22 18:11:59 +0200

    👌 Move credentials store to env dir

--- pkg/tools/secrets/keyring/helpers.go
+++ pkg/tools/secrets/keyring/helpers.go
@@ -1,7 +1,27 @@
 package keyring
 
-import "fmt"
+import (
+	"fmt"
 
-func generateServiceName(environmentName string) string {
-	return fmt.Sprintf("%s-%s", serviceNamePrefix, environmentName)
+	"github.com/99designs/keyring"
+	"github.com/deifyed/xctl/pkg/config"
+)
+
+func (c Client) open() (keyring.Keyring, error) {
+	serviceName := fmt.Sprintf("%s-%s", serviceNamePrefix, c.EnvironmentName)
+
+	environmentDirectory, err := config.GetAbsoluteXCTLClusterDir(c.EnvironmentName)
+	if err != nil {
+		return nil, fmt.Errorf("acquiring environment directory: %w", err)
+	}
+
+	return keyring.Open(keyring.Config{
+		ServiceName:   serviceName,
+		KeychainName:  serviceName,
+		FileDir:       environmentDirectory,
+		KWalletFolder: environmentDirectory,
+		PassDir:       environmentDirectory,
+		PassPrefix:    serviceName,
+		WinCredPrefix: serviceName,
+	})
 }
