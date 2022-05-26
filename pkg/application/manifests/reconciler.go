a78174f0ccff92fb7405132a6f663b1023b27dc2
Julius Pedersen <deifyed@tutanota.com>
2022-05-26 17:59:52 +0200

    ✅ Add deletion of manifests upon delete app

--- pkg/application/manifests/reconciler.go
+++ pkg/application/manifests/reconciler.go
@@ -1,8 +1,11 @@
 package manifests
 
 import (
+	"errors"
 	"fmt"
 	"path"
+
+	"github.com/spf13/afero"
 
 	"github.com/deifyed/xctl/pkg/config"
 
@@ -16,11 +19,8 @@ func (r reconciler) Reconcile(rctx reconciliation.Context) (reconciliation.Resul
 	}
 
 	applicationBaseDir := path.Join(r.absoluteApplicationDir, config.DefaultApplicationBaseDir)
-	applicationOverlaysDir := path.Join(
-		r.absoluteApplicationDir,
-		config.DefaultApplicationsOverlaysDir,
-		rctx.EnvironmentManifest.Metadata.Name,
-	)
+	applicationOverlaysDir := path.Join(r.absoluteApplicationDir, config.DefaultApplicationsOverlaysDir)
+	environmentSpecificOverlaysDir := path.Join(applicationOverlaysDir, rctx.EnvironmentManifest.Metadata.Name)
 
 	switch action {
 	case reconciliation.ActionCreate:
@@ -29,13 +29,41 @@ func (r reconciler) Reconcile(rctx reconciliation.Context) (reconciliation.Resul
 			return reconciliation.Result{}, fmt.Errorf("writing base manifests: %w", err)
 		}
 
-		err = writeOverlaysPatches(rctx.Filesystem, applicationOverlaysDir, rctx.ApplicationDeclaration)
+		err = writeOverlaysPatches(rctx.Filesystem, environmentSpecificOverlaysDir, rctx.ApplicationDeclaration)
 		if err != nil {
 			return reconciliation.Result{}, fmt.Errorf("writing overlays patches: %w", err)
 		}
 
 		return reconciliation.Result{}, nil
 	case reconciliation.ActionDelete:
+		err := rctx.Filesystem.RemoveAll(environmentSpecificOverlaysDir)
+		if err != nil {
+			return reconciliation.Result{}, fmt.Errorf("removing overlays directory: %w", err)
+		}
+
+		exists, err := rctx.Filesystem.Exists(applicationOverlaysDir)
+		if err != nil {
+			return reconciliation.Result{}, fmt.Errorf("checking overlays directory existence: %w", err)
+		}
+
+		if !exists {
+			return reconciliation.Result{}, nil
+		}
+
+		isEmpty, err := rctx.Filesystem.IsEmpty(applicationOverlaysDir)
+		if err != nil && !errors.Is(err, afero.ErrFileNotFound) {
+			return reconciliation.Result{}, fmt.Errorf("checking if overlays directory is empty: %w", err)
+		}
+
+		if !isEmpty {
+			return reconciliation.Result{}, nil
+		}
+
+		err = rctx.Filesystem.RemoveAll(r.absoluteApplicationDir)
+		if err != nil {
+			return reconciliation.Result{}, fmt.Errorf("removing application directory: %w", err)
+		}
+
 		return reconciliation.Result{}, nil
 	}
 
